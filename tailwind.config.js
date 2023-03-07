ca695ca4e81c3819f8567fb1ebe0ff0bcdd35fe1
Hassan-Wanas <test@gamil.com>
2023-03-07 15:32:42 +0200

    html build & add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Gray1: '#E5E5E5',
+        Gray2: '#BDBDBD',
+        Gray3: '#A9A9A9',
+        Gray4: '#4E5150',
+      }
+    },
+    fontFamily: {
+      Montserrat: 'Montserrat',
+    }
+  },
+  plugins: [],
+}
